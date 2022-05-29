0bee7997d509dfdeb8a3663152ecf2a79ebe1e9a
Andrew Hoffman <ahoffmandevelopment@gmail.com>
2022-05-29 10:36:04 -0500

    Update filter-component with column title binding, and comment the crud out of the code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, ElementRef, ViewChild, ViewChildren } from '@angular/core';
 import { MatSelect } from '@angular/material/select';
 import { MatTable } from '@angular/material/table';
-import MovieJson from './MovieData.json';
+import MovieJson from './MovieData.json'; // Define import to JSON file that has movie data.
 
+// Define interface that matches JSON structure
 interface MOVIES {
   id: string;
   popularity: number;
@@ -19,6 +20,7 @@ interface MOVIES {
   relevance: number;
 }
 
+// Set location of items component will use.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -26,32 +28,58 @@ interface MOVIES {
 })
 
 export class AppComponent {
-  @ViewChild("resultsTable") resultsTable!: MatTable<any>;
-  @ViewChild("filterNumber") filterNumber!: ElementRef;
 
+  @ViewChild("resultsTable") resultsTable!: MatTable<any>; // A reference to the results table
+  @ViewChild("filterNumber") filterNumber!: ElementRef; // A reference to number of results selector
+
+  // Map our MoviesJson file to an array of movies
   allMovies: MOVIES[] = MovieJson;
+
+  // Sort our movies by title, then take the top 5 results.
   sortedMovies: MOVIES[] = this.allMovies
     .sort((a,b) => {return a.title.localeCompare(b.title)})
     .slice(0, this.filterNumber?.nativeElement?.value ?? 5);
 
+
+
+  // Get items that will be used to populate the filter components.
+  // These are bound to the filter components in the HTML.
+
+  // Loop through cast items, filtering out null/empty values, sort them, and store them in CastFilterItems
   CastFilterItems: string[] = Array
-    .from(new Set(this.allMovies.flatMap(x => x.cast).flatMap(x => x.trim())))
+    .from(new Set(this.allMovies.flatMap(x => x.cast).flatMap(x => x.trim()).filter(x => x)))
     .sort();
+
+  // Loop through release year items, filtering out null/empty values, sort them, reverse them so they are in descending order,
+  // and store them in ReleaseYearFilterItems
   ReleaseYearFilterItems: string[] = Array
-    .from(new Set(this.allMovies.map(x => x.release_year.trim())))
+    .from(new Set(this.allMovies.map(x => x.release_year.trim()).filter(x => x)))
     .sort()
     .reverse();
+
+  // Loop through director items, filtering out null/empty values, sort them, and store them in DirectorFilterItems
   DirectorFilterItems: string[] = Array
-    .from(new Set(this.allMovies.map(x => x.director.trim())))
+    .from(new Set(this.allMovies.map(x => x.director.trim()).filter(x => x)))
     .sort()
+
+  // Loop through genre items, filtering out null/empty values, sort them, and store them in GenreFilterItems
   GenreFilterItems: string[] = Array
-    .from(new Set(this.allMovies.flatMap(x => x.genres).flatMap(x => x.trim())))
+    .from(new Set(this.allMovies.flatMap(x => x.genres).flatMap(x => x.trim()).filter(x => x)))
     .sort();
 
+
+
+  // Define the display columns the table will use in the html.
   displayColumns: string[] = ['title', 'popularity', 'budget', 'revenue', 'cast', 'homepage', 'director', 'genres', 'production_companies', 'release_year', 'relevance'];
 
+  // A dictionary of the filters that have been requested. The key is the name of the category, such 
+  // as "requestedCastFilters", and the value is the set of filters.
   requestedFilters: Map<string, Set<string>> = new Map<string, Set<string>>();  
 
+
+
+  // Events bound the filter components when check changes. The logic is the same for each:
+  // Add or update the requestedFilters dictionary with the new passed in filters.
   onCastFiltersChanged(filters: any) {
     this.requestedFilters.set("requestedCastFilters", filters)
   }
@@ -68,18 +96,34 @@ export class AppComponent {
     this.requestedFilters.set("requestedGenreFilters", filters);
   }
 
+
+
+  // Filtering function ran when apply filters button is clicked.
   doFiltering() {
 
-    var numOfSelectedCats = 0;
+    // This variable keeps track of the number of categories that have applied filters.
+    // We have to keep track in order to determine the relevance score for each movie.
+    // Start by assuming no filter categories are in use.
+    var numOfSelectedCats = 0; 
     
+    // Loop through the requestedFilters dictionary, and for each entry that has any
+    // filters applied, increment our counter by 1. 
     this.requestedFilters.forEach(x => {
       if (x.size > 0) {
         numOfSelectedCats++;
       }
     }) 
 
-    if (numOfSelectedCats > 0) {
+    if (numOfSelectedCats > 0) { // If any categories have applied filters, enter block
+
+      // Start by setting the relevance for each movie to 0.
       this.allMovies.forEach(x => x.relevance = 0);
+
+
+      // The logic for the next few IF statements is similar.
+      // If the requestedFilters dictionary has any applied filters of the type
+      // being checked, find the movies that match the filter, and increase
+      // their relevance by ten.
 
       if (this.requestedFilters.has("requestedCastFilters")) {
         var castFilters = this.requestedFilters.get("requestedCastFilters");
@@ -125,23 +169,29 @@ export class AppComponent {
         }
       }
 
+      // At this point, because the max relevance that we can have is 40, we need to figure
+      // out how much to increase it to make it correct. Because the amount to increase 
+      // depends on the number of categories with applied filters, we do a small calculation.
       var skew = 100 - (numOfSelectedCats * 10);
 
+      // Loop through all the movies and if they don't have a relevance score of zero (which means
+      // they didn't match any filters) then add the skew to their relevance score to determine
+      // their actual relevance.
       this.allMovies.forEach(x => {
         if (x.relevance > 0 ) {
           x.relevance = x.relevance + skew
         }        
       });      
     } else {
+
+      // If no filter categories are in use, we assume all movies are 100% relevant.
       this.allMovies.forEach(x => x.relevance = 100)
     }
 
+    // Sort the movies first by relevance, then by title, and take the number of results
+    // that user put in the drop down box.
     this.sortedMovies = this.allMovies
       .sort((a,b) => {return b.relevance-a.relevance || a.title.localeCompare(b.title)})
       .slice(0, this.filterNumber?.nativeElement?.value);
-  }
-
-  onMovieClicked(id: string) {
-    window.open(`https://www.imdb.com/title/${id}/`, '_blank')
   }
 }
--- src/app/filter-component/filter-component.component.ts
+++ src/app/filter-component/filter-component.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
+// Set location of items component will use.
 @Component({
   selector: 'app-filter-component',
   templateUrl: './filter-component.component.html',
@@ -8,25 +9,33 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
 
 export class FilterComponentComponent  {
 
-  @Input() FilterItems: string[] = [];
-  @Output() filtersChanged = new EventEmitter<Set<string>>(); 
-  @ViewChild("filterTable") filterTable!: ElementRef;
+  // Define inputs that the componet will use.
+  @Input() FilterItems: string[] = []; // The list of items to display in the component.
+  @Input() DataColumnName: string = ""; // The name of the column to display in the component.
 
+  // Define output.
+  @Output() filtersChanged = new EventEmitter<Set<string>>(); // Event that fires when a checkbox is selected. Listened for on the parent.
+
+  @ViewChild("filterTable") filterTable!: ElementRef; // A reference to the table that we can loop through.
+
+  // Event that fires when a checkbox is selected.
   checkChanged(event: any) {
 
-    var requestedFilters: Set<string> = new Set<string>();
+    // TODO: Could probably improve performance by keeping one static list and adding and removing from it on check changed instead of looping each time.
 
-    var table = this.filterTable.nativeElement as HTMLTableElement;
+    var requestedFilters: Set<string> = new Set<string>(); // New up an empty list of filters
 
-    if(table) {
-      Array.from(table.rows).forEach((row) => {
-        var item = row.cells[1].firstChild as HTMLInputElement;
-        if(item && item.checked) {
+    var table = this.filterTable.nativeElement as HTMLTableElement; // Get reference to table from ViewChild.
+
+    if(table) { // If not null
+      Array.from(table.rows).forEach((row) => { // Loop through rows
+        var item = row.cells[1].firstChild as HTMLInputElement; // Cast the second column to an input element
+        if(item && item.checked) { // If element not null and is checked, add value to filters list.
           requestedFilters.add(item.value);
         }
       });
     }
 
-    this.filtersChanged.emit(requestedFilters);
+    this.filtersChanged.emit(requestedFilters); // Fire event, passing list of filters.
   }
 }
